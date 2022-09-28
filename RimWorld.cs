b3aec01e9f6a50d9d303145c3c94e6c0b488f70f
KatDevsGames <north@tinymagnet.com>
2022-09-28 08:53:13 -0500

    Update RimWorld.cs

--- RimWorld.cs
+++ RimWorld.cs
@@ -4,10 +4,10 @@ using CrowdControl.Common;
 using CrowdControl.Games.Packs;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
-public class RimWorld : SimpleTCPPack {
-
-    public override string Host { get; } = "0.0.0.0";
-    public override ushort Port { get; } = 43384;
+public class RimWorld : SimpleTCPPack
+{
+    public override string Host => "0.0.0.0";
+    public override ushort Port => 43384;
 
     public RimWorld(IPlayer player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
 
@@ -22,8 +22,11 @@ public class RimWorld : SimpleTCPPack {
         new Effect("Healing Grace", "healinggrace","positive"),
         new Effect("New Recruit", "newrecruit","positive"),
         new Effect("Random Gift", "randomgift","positive"),
+        new Effect("Super Gift", "supergift","positive"),
         new Effect("Research Breakthrough", "researchbreakthrough","positive"),
         new Effect("Resurrect Colonist", "resurrectcolonist","positive"),
+        new Effect("Mood Boost", "moodboost","positive"),
+        new Effect("Create Hats", "createhats","positive"),
 
         new Effect("Neutral Effects","neutral", ItemKind.Folder),
         new Effect("Animal Stampede", "animalstampede","neutral"),
@@ -31,6 +34,8 @@ public class RimWorld : SimpleTCPPack {
         new Effect("Raining Cats and Dogs", "catdograin","neutral"),
         new Effect("Random Quest", "randomquest","neutral"),
         new Effect("Trade Caravan", "tradecaravan","neutral"),
+        new Effect("Limb Replacement", "limbreplacement","neutral"),
+        new Effect("Hunter Becomes the Hunted", "hunterbecomeshunted","neutral"),
 
         new Effect("Negative Effects","negative", ItemKind.Folder),
         new Effect("Destroy Hats", "destroyhats","negative"),
@@ -41,6 +46,7 @@ public class RimWorld : SimpleTCPPack {
         new Effect("Tornado", "tornado","negative"),
         new Effect("Wildfire", "wildfire","negative"),
         new Effect("Wildman Horde", "wildmanhorde","negative"),
-
+        new Effect("Foul Food", "foulfood","negative"),
+        new Effect("Power Outage", "poweroutage","negative")
     };
 }
