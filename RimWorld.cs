b855af787b782e0aa9e7145d60608ec190f1c08a
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2023-03-08 19:16:27 +0000

    Crowd Control 2 - Initial Mass Commit

--- RimWorld.cs
+++ RimWorld.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using CrowdControl.Common;
 using CrowdControl.Games.Packs;
 using ConnectorType = CrowdControl.Common.ConnectorType;
@@ -9,44 +8,44 @@ public class RimWorld : SimpleTCPPack
     public override string Host => "0.0.0.0";
     public override ushort Port => 43384;
 
-    public RimWorld(IPlayer player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
+    public RimWorld(Player player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
 
     public override Game Game { get; } = new(160, "RimWorld", "RimWorld", "PC", ConnectorType.SimpleTCPConnector);
 
-    public override List<Effect> Effects { get; } = new()
+    public override EffectList Effects { get; } = new Effect[]
     {
-        new Effect("Positive Effects","positive", ItemKind.Folder),
-        new Effect("Animal Self Tame", "animalselftame","positive"),
-        new Effect("Colony Inspiration", "inspirecolony","positive"),
-        new Effect("Harvest Bountry", "harvestbounty","positive") { Duration = TimeSpan.FromSeconds(300) },
-        new Effect("Healing Grace", "healinggrace","positive"),
-        new Effect("New Recruit", "newrecruit","positive"),
-        new Effect("Random Gift", "randomgift","positive"),
-        new Effect("Super Gift", "supergift","positive"),
-        new Effect("Research Breakthrough", "researchbreakthrough","positive"),
-        new Effect("Resurrect Colonist", "resurrectcolonist","positive"),
-        new Effect("Mood Boost", "moodboost","positive"),
-        new Effect("Create Hats", "createhats","positive"),
+        //new Effect("Positive Effects","positive", ItemKind.Folder),
+        new Effect("Animal Self Tame", "animalselftame") { Category = "Positive" },
+        new Effect("Colony Inspiration", "inspirecolony") { Category = "Positive" },
+        new Effect("Harvest Bountry", "harvestbounty") { Category = "Positive", Duration = TimeSpan.FromSeconds(300) },
+        new Effect("Healing Grace", "healinggrace") { Category = "Positive" },
+        new Effect("New Recruit", "newrecruit") { Category = "Positive" },
+        new Effect("Random Gift", "randomgift") { Category = "Positive" },
+        new Effect("Super Gift", "supergift") { Category = "Positive" },
+        new Effect("Research Breakthrough", "researchbreakthrough") { Category = "Positive" },
+        new Effect("Resurrect Colonist", "resurrectcolonist") { Category = "Positive" },
+        new Effect("Mood Boost", "moodboost") { Category = "Positive" },
+        new Effect("Create Hats", "createhats") { Category = "Positive" },
 
-        new Effect("Neutral Effects","neutral", ItemKind.Folder),
-        new Effect("Animal Stampede", "animalstampede","neutral"),
-        new Effect("Meteorite Crash Landing", "meteoritelanding","neutral"),
-        new Effect("Raining Cats and Dogs", "catdograin","neutral"),
-        new Effect("Random Quest", "randomquest","neutral"),
-        new Effect("Trade Caravan", "tradecaravan","neutral"),
-        new Effect("Limb Replacement", "limbreplacement","neutral"),
-        new Effect("Hunter Becomes the Hunted", "hunterbecomeshunted","neutral"),
+        //new Effect("Neutral Effects","neutral", ItemKind.Folder),
+        new Effect("Animal Stampede", "animalstampede") { Category = "Neutral" },
+        new Effect("Meteorite Crash Landing", "meteoritelanding") { Category = "Neutral" },
+        new Effect("Raining Cats and Dogs", "catdograin") { Category = "Neutral" },
+        new Effect("Random Quest", "randomquest") { Category = "Neutral" },
+        new Effect("Trade Caravan", "tradecaravan") { Category = "Neutral" },
+        new Effect("Limb Replacement", "limbreplacement") { Category = "Neutral" },
+        new Effect("Hunter Becomes the Hunted", "hunterbecomeshunted") { Category = "Neutral" },
 
-        new Effect("Negative Effects","negative", ItemKind.Folder),
-        new Effect("Destroy Hats", "destroyhats","negative"),
-        new Effect("Infestation", "infestation","negative"),
-        new Effect("Mental Break", "mentalbreak","negative"),
-        new Effect("Orbital Barrage", "orbitalbarrage","negative"),
-        new Effect("Outbreak", "outbreak","negative"),
-        new Effect("Tornado", "tornado","negative"),
-        new Effect("Wildfire", "wildfire","negative"),
-        new Effect("Wildman Horde", "wildmanhorde","negative"),
-        new Effect("Foul Food", "foulfood","negative"),
-        new Effect("Power Outage", "poweroutage","negative")
+        //new Effect("Negative Effects","negative", ItemKind.Folder),
+        new Effect("Destroy Hats", "destroyhats") { Category = "Negative" },
+        new Effect("Infestation", "infestation") { Category = "Negative" },
+        new Effect("Mental Break", "mentalbreak") { Category = "Negative" },
+        new Effect("Orbital Barrage", "orbitalbarrage") { Category = "Negative" },
+        new Effect("Outbreak", "outbreak") { Category = "Negative" },
+        new Effect("Tornado", "tornado") { Category = "Negative" },
+        new Effect("Wildfire", "wildfire") { Category = "Negative" },
+        new Effect("Wildman Horde", "wildmanhorde") { Category = "Negative" },
+        new Effect("Foul Food", "foulfood") { Category = "Negative" },
+        new Effect("Power Outage", "poweroutage") { Category = "Negative" }
     };
 }
